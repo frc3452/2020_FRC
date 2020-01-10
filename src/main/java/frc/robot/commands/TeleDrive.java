17aaa49a7d301360c99af9bd068acef00666e49d
BrendanNestle <brendannestle@gmail.com>
2020-01-10 17:54:08 -0500

    DriveForTIme added

--- src/main/java/frc/robot/commands/TeleDrive.java
+++ src/main/java/frc/robot/commands/TeleDrive.java
@@ -1,3 +1,4 @@
+//There's nothing changed in here
 /*----------------------------------------------------------------------------*/
 /* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
@@ -7,56 +8,34 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.DriveTrain;
 
-public class DriveForTime extends CommandBase {
-  private final DriveTrain m_DriveTrain;
-
-  private double startTime;
-
-  private double time;
-
-  private double rotate;
-
-  private double move;
-
+public class TeleDrive extends CommandBase {
   /**
-   * Creates a new DriveForTime.
+   * Creates a new TeleDrive.
    */
-  public DriveForTime(DriveTrain driveTrain, double move, double rotate, double time) {
-    m_DriveTrain = driveTrain;
-    this.move = move;
-    this.rotate = rotate;
-    this.time = time;
-
-    addRequirements(m_DriveTrain);
+  public TeleDrive() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    startTime = Timer.getFPGATimestamp();
-  } 
-
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_DriveTrain.arcadeDriveControl(move, rotate, false);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_DriveTrain.arcadeDriveControl(0, 0, false);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return (Timer.getFPGATimestamp() - startTime) >= time;
+    return false;
   }
 }
