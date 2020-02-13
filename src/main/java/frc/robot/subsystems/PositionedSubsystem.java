3459214c8affbcd46c16cdb1377efca2896ed2e1
maccopacco <maxdmax101@gmail.com>
2020-02-13 17:10:05 -0500

    Cleaned up RobotContainer and PositionedSubsystem example

---
+++ src/main/java/frc/robot/subsystems/PositionedSubsystem.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.kOuttake.OuttakePositions;
+
+public class PositionedSubsystem extends SubsystemBase {
+    //declaration of motor controllers
+//    private final Spark spark;
+    public PositionedSubsystem() {
+//        spark = new Spark(1);
+    }
+
+    public void setPosition(OuttakePositions position) {
+        //
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+}
